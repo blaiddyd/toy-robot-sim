4384f838bf11d34324274c14fe99f63f83e6722f
ekinbukulmez <22412569@student.uwa.edu.au>
2019-06-25 20:12:37 +0800

    more functions

--- robot_functions.c
+++ robot_functions.c
@@ -2,16 +2,23 @@
 
 struct Robot init_robot() {
     int* position = malloc(sizeof(int) * 2);
-    char* direction = malloc(sizeof(char) * 6);
+    char* direction = malloc(sizeof(char) * 20);
     direction[0] = '\0';
 
     position[0] = 0;
-    position[1] = 1;
-
-    sprintf(direction, "RIGHT");
+    position[1] = 0;
 
     struct Robot toy_robot = { position, direction };
 
     return toy_robot;
   
+}
+
+void eval_command(char* command, struct Robot toy_robot) {
+    // check for place command
+
+}
+
+void change_position(char* pos, struct Robot toy_robot) {
+    
 }
--- toy_robot.c
+++ toy_robot.c
@@ -1,9 +1,12 @@
 #include "robot.h"
 
-int main() {
-    struct Robot toy_robot = init_robot();
+int main(int argc, char **argv) {
+    int test_flag = 0;
+    char *command = malloc(sizeof(char) * 20);
 
     printf("Welcome to Ekin's Toy Robot Simulator.\n");
+
+    struct Robot toy_robot = init_robot();
 
     return 0;
 }
